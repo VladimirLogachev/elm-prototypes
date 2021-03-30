b948be7208368b56e3f38a34059c8a77191605a7
Vladimir Logachev <logachev.dev@ya.ru>
2021-03-30 13:13:04 +0700

    Run the same tests without Percy

--- 001_build_and_run_in_docker/integration-tests/cypress/integration/routing.test.js
+++ 001_build_and_run_in_docker/integration-tests/cypress/integration/routing.test.js
@@ -5,13 +5,13 @@ context('SPA routing - should contain current page address', () => {
     cy.visit('/page-a')
     cy.get('html').should('contain', 'The current path is: /page-a')
     cy.get('html').should('not.contain', 'The current path is: /page-b')
-    cy.percySnapshot('page-a', { widths: [768] });
+    // cy.percySnapshot('page-a', { widths: [768] });
   })
 
   it('page-b', () => {
     cy.visit('/page-b')
     cy.get('html').should('contain', 'The current path is: /page-b')
     cy.get('html').should('not.contain', 'The current path is: /page-a')
-    cy.percySnapshot('page-b', { widths: [768] });
+    // cy.percySnapshot('page-b', { widths: [768] });
   })
 })
